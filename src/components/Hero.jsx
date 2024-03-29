c799dd4da2689143a13d10c784751a503985c35b
AlgorLBK <algorlombako@gmail.com>
2024-03-29 00:08:24 -0400

    Fifth project 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Algor</span>
+            Hi, 'm <span className='text-[#915EFF]'>Algor</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop mobile applications, web 
