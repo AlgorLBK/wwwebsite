60e5d9441551519a6321be8bd10e61bc0db8f7ec
Algor Lombako <algorlombako@gmail.com>
2026-01-01 20:36:20 -0500

    insta cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react"; // ✅ Add this
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Capstone, Socials } from "./components";
 
 const App = () => {
+
+  const CleanURL = () => {
+    useEffect(() => {
+      const url = new URL(window.location.href);
+      if (url.search) {
+        window.history.replaceState({}, document.title, url.pathname);
+      }
+    }, []);
+    return null;
+  }
+
   return (
     <BrowserRouter>
+      <CleanURL />
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
